bc62ded8d305f1e7c58062e101db1b086de208f3
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2021-02-07 16:16:32 -0300

    implement delegate

--- src/main/java/fit/core/SpendRepository.java
+++ src/main/java/fit/core/SpendRepository.java
@@ -1,7 +1,24 @@
 package fit.core;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class SpendRepository extends ArrayList<Spend> {
-    // none here (b/c we are reusing everything from ArrayList)
+public class SpendRepository {
+    private final List<Spend> all;
+
+    public SpendRepository() {
+        this.all = new ArrayList<Spend>();
+    }
+
+    public boolean add(Spend spend) {
+        return all.add(spend);
+    }
+
+    public boolean contains(Spend spend) {
+        return this.all.contains(spend);
+    }
+
+    public int size() {
+        return this.all.size();
+    }
 }
