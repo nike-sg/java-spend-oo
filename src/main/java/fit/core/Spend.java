b667d3be8548bdd6fe0a5563471c1b3417c661db
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2021-02-07 16:12:52 -0300

    implement with inheritance

---
+++ src/main/java/fit/core/Spend.java
@@ -0,0 +1,33 @@
+package fit.core;
+
+import java.util.Date;
+
+public class Spend {
+    private String name;
+    private Date date;
+    private double value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/fit/core/SpendRepository.java
@@ -0,0 +1,7 @@
+package fit.core;
+
+import java.util.ArrayList;
+
+public class SpendRepository extends ArrayList<Spend> {
+    // none here (b/c we are reusing everything from ArrayList)
+}
